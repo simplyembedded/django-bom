3c1f5284a920ba4c1ed4b53a339f3e0dfaae89f2
Willis <williskneeland1234@gmail.com>
2025-04-27 19:58:09 -0600

    Add email to settings, no credentials set

--- bom/settings.py
+++ bom/settings.py
@@ -22,9 +22,13 @@ ODOO_PASSWORD   = 'Arin2007'
 ODOO_COMMON_URL = f"{ODOO_URL}/xmlrpc/2/common"
 ODOO_OBJECT_URL = f"{ODOO_URL}/xmlrpc/2/object"
 
-
-
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
