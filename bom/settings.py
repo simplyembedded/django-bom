798506a74f878918b4651302e7a90b0f3f911d9f
Willis <williskneeland1234@gmail.com>
2025-05-01 19:28:25 -0600

    prepopulate admin email in settings

--- bom/settings.py
+++ bom/settings.py
@@ -24,7 +24,7 @@ ODOO_OBJECT_URL = f"{ODOO_URL}/xmlrpc/2/object"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
+EMAIL_HOST_USER = 'admin@simplyembedded.ca'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
